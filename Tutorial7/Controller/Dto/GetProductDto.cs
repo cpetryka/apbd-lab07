9fa993f91ffa46e460bec7e43bcd269b15fcec3b
Cezary Petryka <cezaryp10@gmail.com>
2024-05-10 15:59:55 +0200

    Add a DTO for fetching a product from db

---
+++ Tutorial7/Controller/Dto/GetProductDto.cs
@@ -0,0 +1,9 @@
+namespace Tutorial7.Controller.Dto;
+
+public class GetProductDto
+{
+    public int IdProduct { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+}
